1a5be3f67a2e83d1f229d8541dd256a31ded432e
Egil <egil.moller@freecode.no>
2018-05-29 14:28:38 +0200

    Removed cloudberry from inside radiusdocker

--- radius/db/content.sql
+++ radius/db/content.sql
@@ -1,23 +1,23 @@
-insert into radcheck(UserName, Attribute, Value, Op) values
-  ('fredf','Cleartext-Password','wilma',':='),
-  ('barney','Cleartext-Password','betty',':='),
-  ('dialrouter','Cleartext-Password','dialup',':=');
+insert into radcheck(Created, Modified, UserName, Attribute, Value, Op, is_active) values
+  ('now', 'now', 'fredf','Cleartext-Password','wilma',':=', true),
+  ('now', 'now', 'barney','Cleartext-Password','betty',':=', true),
+  ('now', 'now', 'dialrouter','Cleartext-Password','dialup',':=', true);
 
-insert into radreply (UserName, Attribute, Value, Op) values
-  ('barney','Framed-IP-Address','1.2.3.4',':='),
-  ('dialrouter','Framed-IP-Address','2.3.4.1',':='),
-  ('dialrouter','Framed-IP-Netmask','255.255.255.255',':='),
-  ('dialrouter','Framed-Routing','Broadcast-Listen',':='),
-  ('dialrouter','Framed-Route','2.3.4.0 255.255.255.248',':='),
-  ('dialrouter','Idle-Timeout','900',':=');
+insert into radreply (Created, Modified, UserName, Attribute, Value, Op) values
+  ('now', 'now', 'barney','Framed-IP-Address','1.2.3.4',':='),
+  ('now', 'now', 'dialrouter','Framed-IP-Address','2.3.4.1',':='),
+  ('now', 'now', 'dialrouter','Framed-IP-Netmask','255.255.255.255',':='),
+  ('now', 'now', 'dialrouter','Framed-Routing','Broadcast-Listen',':='),
+  ('now', 'now', 'dialrouter','Framed-Route','2.3.4.0 255.255.255.248',':='),
+  ('now', 'now', 'dialrouter','Idle-Timeout','900',':=');
 
-insert into radgroupreply (GroupName, Attribute, Value, Op) values
-  ('dynamic','Framed-Compression','Van-Jacobsen-TCP-IP',':='),
-  ('dynamic','Framed-Protocol','PPP',':='),
-  ('dynamic','Service-Type','Framed-User',':='),
-  ('dynamic','Framed-MTU','1500',':='),
-  ('static','Framed-Protocol','PPP',':='),
-  ('static','Service-Type','Framed-User',':='),
-  ('static','Framed-Compression','Van-Jacobsen-TCP-IP',':='),
-  ('netdial','Service-Type','Framed-User',':='),
-  ('netdial','Framed-Protocol','PPP',':=');
+insert into radgroupreply (Created, Modified, GroupName, Attribute, Value, Op) values
+  ('now', 'now', 'dynamic','Framed-Compression','Van-Jacobsen-TCP-IP',':='),
+  ('now', 'now', 'dynamic','Framed-Protocol','PPP',':='),
+  ('now', 'now', 'dynamic','Service-Type','Framed-User',':='),
+  ('now', 'now', 'dynamic','Framed-MTU','1500',':='),
+  ('now', 'now', 'static','Framed-Protocol','PPP',':='),
+  ('now', 'now', 'static','Service-Type','Framed-User',':='),
+  ('now', 'now', 'static','Framed-Compression','Van-Jacobsen-TCP-IP',':='),
+  ('now', 'now', 'netdial','Service-Type','Framed-User',':='),
+  ('now', 'now', 'netdial','Framed-Protocol','PPP',':=');
--- radius/db/users.sql
+++
@@ -1,4 +0,0 @@
-create database radius;
-create user radius;
-alter role radius with password 'radpass';
-grant all on database radius to radius;
