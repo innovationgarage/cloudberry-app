f2dd2cb2074a8a70b2b27d1fd349fa8df9cc8354
Egil <egil.moller@freecode.no>
2018-07-01 17:44:29 +0200

    Lots of bugfixes

--- download-images/source_images/download.sh
+++ download-images/source_images/download.sh
@@ -31,7 +31,7 @@ download () {
 
 # Format is "name image repo offset", one entry per line
 download <<EOF
-OrangePi-R1 https://openwrt.innovationgarage.no/targets/sunxi/cortexa7/openwrt-sunxi-cortexa7-sun8i-h2-plus-orangepi-r1-ext4-sdcard.img.gz https://github.com/innovationgarage/cloudberry-device-orangepi.git 23068672
-RaspberryPI-1 https://openwrt.innovationgarage.no/targets/brcm2708/bcm2708/lede-brcm2708-bcm2708-rpi-ext4-sdcard.img https://github.com/innovationgarage/cloudberry-device-orangepi.git 23068672
-RaspberryPI-2 https://openwrt.innovationgarage.no/targets/brcm2708/bcm2709/lede-brcm2708-bcm2709-rpi-2-ext4-sdcard.img.gz https://github.com/innovationgarage/cloudberry-device-orangepi.git 23068672
+OrangePi-R1 https://openwrt.innovationgarage.no/targets/sunxi/cortexa7/openwrt-sunxi-cortexa7-sun8i-h2-plus-orangepi-r1-ext4-sdcard.img.gz https://github.com/innovationgarage/cloudberry-device.git 23068672
+RaspberryPI-1 https://openwrt.innovationgarage.no/targets/brcm2708/bcm2708/lede-brcm2708-bcm2708-rpi-ext4-sdcard.img https://github.com/innovationgarage/cloudberry-device.git 23068672
+RaspberryPI-2 https://openwrt.innovationgarage.no/targets/brcm2708/bcm2709/lede-brcm2708-bcm2709-rpi-2-ext4-sdcard.img.gz https://github.com/innovationgarage/cloudberry-device.git 23068672
 EOF
